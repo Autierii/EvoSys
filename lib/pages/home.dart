1c308fad507615a89850ce858e3fcd904271d28c
Rafael <rafael_autieri@hotmail.com>
2025-07-01 14:45:43 -0300

    Barra de navegação nao funcional

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,49 +7,96 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: _buildAppBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+              color: Color(0xff1D1617),
+              blurRadius: 40,
+              spreadRadius: 0.0
+                )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromARGB(255, 232, 244, 255),
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset(
+                    'assets/icons/search.svg',
+                    height: 20,
+                    width: 20,
+                    ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                )
+              )
+            ),
+          )
+        ]
+      ),
+    );
+  }
+
+
+    AppBar _buildAppBar() {
+      return AppBar(
         title: Text(
           'Evo Systems',
-              style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.bold
-            ),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
-          backgroundColor: Colors.blueAccent,
-          elevation: 0.0,
-          centerTitle: true,
-          leading: Container(
-            margin: EdgeInsets.all(10),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10)
-            ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 56, 126, 255),
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              
+            },
             child: SvgPicture.asset(
               'assets/icons/left-arrow-svgrepo-com.svg',
               height: 20,
               width: 20,
-            )
+            ),
           ),
-
-          actions: [
-            Container(
+        ),
+        actions: [
+          Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
-            // ignore: sort_child_properties_last
-            child: SvgPicture.asset(
-              'assets/icons/dots-horizontal-alt-svgrepo-com.svg',
-              width: 37,
-            ),
             decoration: BoxDecoration(
               color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10)
-            )
-          )
-          ],
-
-        )
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: GestureDetector(
+              onTap: () {
+                
+              },
+              child: SvgPicture.asset(
+                'assets/icons/dots-horizontal-alt-svgrepo-com.svg',
+                width: 37,
+              ),
+            ),
+          ),
+        ],
       );
     }
   }
