aba63c034194d17dc153138d933711fee2544ee5
Rafael <rafael_autieri@hotmail.com>
2025-07-02 21:15:53 -0300

    Reponsividade arrumada para android

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,73 +10,71 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-    class _HomeScreenState extends State<HomeScreen> {
-      final ApiService _apiService = ApiService();
-          List<Movie> _movies = [];
-        final TextEditingController _controller = TextEditingController();
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiService _apiService = ApiService();
+  List<Movie> _movies = [];
+  final TextEditingController _controller = TextEditingController();
 
   @override
-    void initState() {
-        super.initState();
-     _loadPopularMovies();
-      }
+  void initState() {
+    super.initState();
+    _loadPopularMovies();
+  }
 
-      void _loadPopularMovies() async {
-        final movies = await _apiService.fetchPopularMovies();
-        setState(() {
-          _movies = movies;
-      }
-      );
-
+  void _loadPopularMovies() async {
+    final movies = await _apiService.fetchPopularMovies();
+    setState(() {
+      _movies = movies;
+    });
   }
 
   void _searchMovies(String query) async {
-        if (query.isEmpty) {
-          _loadPopularMovies();
-        } else {
-            final movies = await _apiService.searchMovies(query);
-        setState(() {
-          _movies = movies;
-        });
+    if (query.isEmpty) {
+      _loadPopularMovies();
+    } else {
+      final movies = await _apiService.searchMovies(query);
+      setState(() {
+        _movies = movies;
+      });
     }
   }
 
   @override
-     Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Filmes'),
-            leading: Container(
-            margin: EdgeInsets.all(10),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10)
-                ),
-            ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Evo Sys'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.blueAccent,
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
         ),
-      
+      ),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: TextField(
               controller: _controller,
-              onChanged: _searchMovies,
               decoration: const InputDecoration(
-               hintText: 'Buscar filmes...',
-                prefixIcon: Icon(Icons.search),
+                labelText: 'Buscar filme',
                 border: OutlineInputBorder(),
               ),
+              onSubmitted: _searchMovies,
             ),
           ),
-              Expanded(
-              child: GridView.builder(
-                   itemCount: _movies.length,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                childAspectRatio: 0.6,
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(12),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 0.65,
               ),
+              itemCount: _movies.length,
               itemBuilder: (context, index) {
                 final movie = _movies[index];
                 return GestureDetector(
@@ -86,34 +84,35 @@ class HomeScreen extends StatefulWidget {
                       MaterialPageRoute(
                         builder: (_) => DetailsPage(movie: movie),
                       ),
-                      );
-                       },
-                   child: Card(
-                           child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                            Expanded(
-                                child: Image.network(
-                              'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                              fit: BoxFit.cover,
-                             ),
-                          ),
-                          Padding(
-                          padding: const EdgeInsets.all(4.0),
-                           child: Text(
-                             movie.title,
-                             maxLines: 2,
-                             overflow: TextOverflow.ellipsis,
+                    );
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                            fit: BoxFit.cover,
                           ),
                         ),
-                          ],
-                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        movie.title,
+                        style: const TextStyle(fontSize: 14),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
-                   );
-                 },
-                ),
-              ),
-          ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
